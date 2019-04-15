8702ef2ed20d762a2738abd2485a444ef891ec50
Max <m.taubert2015@gmail.com>
2019-04-15 17:06:56 +0100

    UI and multi map generation

--- Scripts/Terrain_Generator.gd
+++ Scripts/Terrain_Generator.gd
@@ -2,18 +2,28 @@ extends Node
 
 var gridSize = Vector2(100, 53)
 var grids = {}
-var entriesPerIteration = 1
+var entriesPerIteration = 5
+var noise = {}
 
 func _ready():
 	randomize()
 
+func configure_noise(octaves, period, persistence):
+	for i in range(entriesPerIteration):
+		noise[i] = OpenSimplexNoise.new()
+		noise[i].seed = randi()
+		noise[i].octaves = octaves
+		noise[i].period = period
+		noise[i].persistence = persistence
+
 func get_next_iteration():
+	configure_noise(4, 20, 0.8)
 	for i in range(entriesPerIteration):
 		var grid = []
 		for x in range(gridSize.x):
 			grid.append([])
 			for y in range(gridSize.y):
-				grid[x].append(randi()%2)
+				grid[x].append(randi()%7)
 		grids[i] = grid
 	
 	return grids
--- Scripts/Terrain_Map.gd
+++ Scripts/Terrain_Map.gd
@@ -4,14 +4,34 @@ onready var terrain = get_node("Terrain")
 
 var gridSize
 var currentIterationGrids
+var numberOfMaps
+var currentMapindex
 
 func _ready():
 	gridSize = Terrain_Generator.gridSize
 	currentIterationGrids = Terrain_Generator.get_next_iteration()
+	numberOfMaps = currentIterationGrids.size()
+	currentMapindex = 0
 	display_maps()
 
 func display_maps():
-	print(currentIterationGrids[0])
+	$UI/Menu_Bar/Map_Number.text = "Map: " + String(currentMapindex+1)
 	for x in range(gridSize.x):
 		for y in range(gridSize.y):
-			terrain.set_cell(x,y,currentIterationGrids[0][x][y])
+			terrain.set_cell(x,y,currentIterationGrids[currentMapindex][x][y])
+
+#Switch to the previous map
+func _on_Left_Button_pressed():
+	currentMapindex -= 1
+	if currentMapindex < 0:
+		currentMapindex = numberOfMaps-1
+	
+	display_maps()
+
+#Switch to the next map
+func _on_Right_Button_pressed():
+	currentMapindex += 1
+	if currentMapindex == numberOfMaps:
+		currentMapindex = 0
+	
+	display_maps()
